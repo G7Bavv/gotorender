84c5c7133d0760379638fb5d86745ac497312cc6
G7Bavv <g7bprogamer@gmail.com>
2025-11-05 03:11:35 +0100

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,14 +1,14 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1397175895587946537",
+        channelId: "1368359602286231552",
         serverId: "1338237780664254504",
         token: process.env.token1,
         selfDeaf: true,
         autoReconnect: {
             enabled: true,
             delay: 6, // ثواني
-            maxRetries: 10,
+            maxRetries: 15,
         },
         presence: {
             status: "idle",
@@ -17,3 +17,4 @@ export default [
     },
 
 ];
+
