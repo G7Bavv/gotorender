3869bf360fc1256868168cad3ad832d53088abd9
G7Bavv <g7bprogamer@gmail.com>
2025-12-09 20:04:17 +0100

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1447999808945262602",
+        channelId: "1448026893684178995",
         serverId: "1440746839438655531",
         token: process.env.token1,
         selfDeaf: true,
@@ -17,6 +17,7 @@ export default [
     },
 
 ];
+
 
 
 
