2213854aa9f75b97016e33e1d90a2185192779b3
G7Bavv <g7bprogamer@gmail.com>
2026-03-19 04:37:12 +0000

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -4,7 +4,7 @@ export default [
         channelId: "1483844929510969455",
         serverId: "1437717487750742018",
         token: process.env.token1,
-        selfDeaf: true,
+        selfDeaf: false,
         autoReconnect: {
             enabled: true,
             delay: 17, // ثواني
@@ -13,7 +13,7 @@ export default [
         presence: {
             status: "dnd",
         },
-        selfMute: true,
+        selfMute: false,
     },
 
 ];
